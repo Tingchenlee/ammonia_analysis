8f6804fcae3d6ab2a8502e9dcc1a5937fe3995fd
Tingchenlee <lee.ting@northeastern.edu>
2021-05-13 00:12:32 -0400

    added Chris' PFR script and remove .csv .png .dot .pdf files

---
+++ ammonia_PFR_array_job.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=1:00:00
+#SBATCH --mem=20GB
+#SBATCH --job-name=ammonia
+#SBATCH --output=logs/ammonia_Rebrov.%a.log
+#SBATCH --error=logs/ammonia_Rebrov.%a.slurm.log
+#SBATCH --partition=short
+
+#an array for the job.
+#SBATCH --array=1-107
+
+
+####################################################
+source activate rmg_env
+python -u ammonia_PRF_reactor_script.py
