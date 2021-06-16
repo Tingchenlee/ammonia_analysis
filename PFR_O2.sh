c8966d4a92183032bd22df4aad6d0e5102576961
Tingchenlee <lee.ting@northeastern.edu>
2021-06-15 22:18:25 -0400

    cleaned up old folder and added the no_gasrxn script

---
+++ PFR_O2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=1:00:00
+#SBATCH --mem=20GB
+#SBATCH --job-name=ammonia_O2
+#SBATCH --output=logs/ammonia_Rebrov_O2/ammonia_Rebrov_O2.%a.log
+#SBATCH --error=logs/ammonia_Rebrov_O2/ammonia_Rebrov_O2.%a.slurm.log
+#SBATCH --partition=short
+#SBATCH --mail-user=lee.ting@northeastern.edu
+#SBATCH --mail-type=FAIL,END
+
+#an array for the job.
+#SBATCH --array=1-99
+
+
+####################################################
+source activate rmg_env
+python -u ammonia_PFR_reactor_script_O2.py
---
+++ PFR_temp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=1:00:00
+#SBATCH --mem=20GB
+#SBATCH --job-name=ammonia_temp
+#SBATCH --output=logs/ammonia_Rebrov_temp/ammonia_Rebrov_temp.%a.log
+#SBATCH --error=logs/ammonia_Rebrov_temp/ammonia_Rebrov_temp.%a.slurm.log
+#SBATCH --partition=short
+#SBATCH --mail-user=lee.ting@northeastern.edu
+#SBATCH --mail-type=FAIL,END
+
+#an array for the job.
+#SBATCH --array=1-99
+
+
+####################################################
+source activate rmg_env
+python -u ammonia_PFR_reactor_script_temp.py
